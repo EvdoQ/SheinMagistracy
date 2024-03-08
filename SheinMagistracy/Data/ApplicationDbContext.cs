a7003946aaea8e27db8ce8c1f8543b26f1295a2e
E. Borodov <mgopjik@gmail.com>
2024-03-08 09:38:29 +0300

    Create Subject model, add local DbConnection

---
+++ SheinMagistracy/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SheinMagistracy.Models;
+
+namespace SheinMagistracy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        DbSet<Subject> Subject {  get; set; }
+    }
+}
---
+++ SheinMagistracy/Models/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SheinMagistracy.Models
+{
+    public class Subject
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SubjectName { get; set; }
+        public string Teacher { get; set; }
+    }
+}
